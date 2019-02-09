56cf688a0a23a84b38042bf288d2ac552e254b72
yseki12 <yosuke.seki@gmail.com>
2019-02-09 16:02:52 -0600

    Add files via upload

---
+++ InputFile.py
@@ -0,0 +1,19 @@
+from tkinter import messagebox, filedialog
+from tkinter import *
+
+root = Tk()
+root.withdraw()
+
+def importFile():
+    
+    messagebox.showinfo('Please Select A File', 'Please Select A File')
+    filename = filedialog.askopenfilename(initialdir = "/",title = "Select file",
+                                      filetypes = (("CSV files","*.csv"),("All Files","*.*")))
+    
+    if not filename:
+        print('No file')
+        root.destroy()
+    else:
+        return filename
+
+importFile()
